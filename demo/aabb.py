f6e0b0df0d039a5ac9fda38a190c4311e7c7b475
候营辉 <yinghuihou@foxmail.com>
2021-03-19 10:46:12 +0800

    delete

--- demo/aabb.py
+++
@@ -1,9 +0,0 @@
-from li import ddd as drr
-
-def add(x, y):
-    print("x和y的和为：%d" % (x + y))
-
-    drr.haha()
-
-
-
--- demo/one.py
+++
@@ -1,9 +0,0 @@
-import aabb
-from list import ccc
-
-
-def add(x, y):
-    print("x和y的和为：%d" % (x + y))
-    aabb.add(3, 5)
-    ccc.haha()
-
